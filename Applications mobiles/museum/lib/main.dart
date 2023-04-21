ba3588d77d81fb089cdae9c310a37be256034bd1
Maxence <43523385+MaxenceBgn@users.noreply.github.com>
2023-04-21 22:40:30 +0200

    Ré-organisation des widget
    - Renommage du widget "Body" en "Artwork"
    - Basculement du widget "Artwork" en StateFulWidget au lieu de StateLessWidget

--- Applications mobiles/museum/lib/main.dart
+++ Applications mobiles/museum/lib/main.dart
@@ -1,65 +1,95 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
+}
+
+class Header extends StatelessWidget implements PreferredSizeWidget {
+  const Header({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: const Text('Museum'),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
+
+class Artwork extends StatefulWidget {
+  const Artwork({Key? key}) : super(key: key);
+
+  @override
+  _ArtworkState createState() => _ArtworkState();
+}
+
+class _ArtworkState extends State<Artwork> {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Row(
+            children: [
+              Container(
+                padding: const EdgeInsets.only(bottom: 4),
+                child: const Text(
+                  "Mona Lisa",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.brown,
+                    fontSize: 30,
+                    fontFamily: 'Merriweather',
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const Row(
+            children: [
+              Text(
+                "Léonard de Vinci",
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.brown,
+                ),
+              )
+            ],
+          ),
+          const Row(
+            children: [
+              Column(
+                children: [Icon(Icons.article)],
+              ),
+              Column(
+                children: [Icon(Icons.favorite)],
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Application de la Joconde',
-        theme: ThemeData(
-          primarySwatch: Colors.brown,
-        ),
-        home: Scaffold(
-            appBar: AppBar(
-              title: const Text('Museum'),
-            ),
-            body: SingleChildScrollView(
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Row(
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.only(bottom: 4),
-                          child: const Text(
-                            "Mona Lisa",
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Colors.brown,
-                                fontSize: 30,
-                                fontFamily: 'Merriweather'),
-                          ),
-                        ),
-                      ],
-                    ),
-                    const Row(
-                      children: [
-                        Text(
-                          "Léonard de Vinci",
-                          style: TextStyle(
-                              fontSize: 15,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.brown),
-                        )
-                      ],
-                    ),
-                    const Row(
-                      children: [
-                        Column(
-                          children: [Icon(Icons.article)],
-                        ),
-                        Column(
-                          children: [Icon(Icons.favorite)],
-                        )
-                      ],
-                    )
-                  ]),
-            )));
+      title: 'Application de la Joconde',
+      theme: ThemeData(
+        primarySwatch: Colors.brown,
+      ),
+      home: const Scaffold(
+        appBar: Header(),
+        body: Artwork(),
+      ),
+    );
   }
 }
