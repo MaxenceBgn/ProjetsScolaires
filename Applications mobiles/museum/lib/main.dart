ec0fa1d3068864d8c60af6643d7b6415f873f879
Maxence <43523385+MaxenceBgn@users.noreply.github.com>
2023-04-25 16:11:14 +0200

    Ajout texte de l'article
    - Ajout du texte de l'article de la Joconde
    - Possibilité d'afficher ou masquer le texte en cliquant sur l'icône "article"

--- Applications mobiles/museum/lib/main.dart
+++ Applications mobiles/museum/lib/main.dart
@@ -29,27 +29,16 @@ class Artwork extends StatefulWidget {
 
 class _ArtworkState extends State<Artwork> {
   bool isFavorite = false;
-  int numberOfLikes = 55;
-  String alertText = "";
-  Color alertColor = Colors.white;
+  bool _showDescription = false;
 
-  void _clickOnFavorite() {
-    if (isFavorite == true) {
-      setState(() {
-        isFavorite = false;
-        numberOfLikes--;
-        alertText = "Oeuvre supprimée de vos favoris.";
-        alertColor = Colors.red;
-      });
-    } else {
-      setState(() {
-        isFavorite = true;
-        numberOfLikes++;
-        alertText = "Oeuvre ajoutée à vos favoris.";
-        alertColor = Colors.green;
-      });
-    }
+  int numberOfLikes = 55; //favoris
+  Color alertColor = Colors.transparent;
 
+  String _articleText =
+      "La Joconde, ou portrait de Mona Lisa, est un tableau de l'artiste Léonard de Vinci, réalisé entre 1503 et 1506 ou entre 1513 et 1516, et peut être jusqu'à 1517 (l'artiste étant mort le 2 mai 1519), qui représente un portrait mi-corps, probablement celui de la Florentine Lisa Gherardini, épouse de Francesco del Giocondo. Acquise par François Ier, cette peinture à l'huile sur panneau de bois de peuplier de 77 x 53cm est exposée au musée du Louvres à Paris. La Joconde est l'un des rares tableaux attribués de façon certaine à Léonard de Vinci. \n\nLa Joconde est devenue un tableau éminemment célèbre car, depuis sa réalisation, nombre d'artistes l'ont pris comme référence. A l'époque romantique, les artistes ont été fascinés par ce tableau et ont contribués à développer le mythe qui l'entoure, en faisant de ce tableau l'une des oeuvres d'art les plus célèbres au monde, si ce n'est la plus célèbre : elle est en tout cas considérée comme l'une des représentations d'un visage féminin les plus célèbres au monde. Au XXie siècle, elle est devenue l'objet d'art le plus visité au monde, devant le Diamant Hope, avec 20 000 visiteurs qui viennent l'admirer et la photographier quotidiennement.";
+  Color articleColor = Colors.transparent;
+
+  void _displayAlert(String alertText) {
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
         content: Text(alertText),
@@ -57,6 +46,38 @@ class _ArtworkState extends State<Artwork> {
         backgroundColor: alertColor,
       ),
     );
+  }
+
+  void _clickOnFavorite() {
+    if (isFavorite == true) {
+      setState(() {
+        isFavorite = false;
+        numberOfLikes--;
+        alertColor = Colors.red;
+        _displayAlert("Oeuvre supprimée de vos favoris.");
+      });
+    } else {
+      setState(() {
+        isFavorite = true;
+        numberOfLikes++;
+        alertColor = Colors.green;
+        _displayAlert("Oeuvre ajoutée à vos favoris.");
+      });
+    }
+  }
+
+  void _clickOnArticle() {
+    if (_showDescription == false) {
+      setState(() {
+        _showDescription = true;
+        articleColor = Colors.white;
+      });
+    } else {
+      setState(() {
+        _showDescription = false;
+        articleColor = Colors.transparent;
+      });
+    }
   }
 
   @override
@@ -81,6 +102,30 @@ class _ArtworkState extends State<Artwork> {
                     onPressed: () {
                       _clickOnFavorite();
                     }),
+              ),
+              Positioned(
+                top: (MediaQuery.of(context).size.height - 350) / 3,
+                left: (MediaQuery.of(context).size.width - 300) / 2,
+                child: Container(
+                  width: 300,
+                  height: 350,
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Positioned(
+                      child: Container(
+                        width: 300,
+                        height: 350,
+                        child: SingleChildScrollView(
+                          child: Text(
+                            _articleText,
+                            style: TextStyle(fontSize: 15, color: articleColor),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
@@ -132,10 +177,14 @@ class _ArtworkState extends State<Artwork> {
                     mainAxisAlignment: MainAxisAlignment.center,
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      Icon(
-                        Icons.article,
-                        color: Colors.brown,
-                      )
+                      IconButton(
+                          onPressed: () {
+                            _clickOnArticle();
+                          },
+                          icon: Icon(
+                            Icons.article,
+                            color: isFavorite ? Colors.red : Colors.brown,
+                          )),
                     ],
                   ),
                   SizedBox(width: MediaQuery.of(context).size.width * 0.3),
