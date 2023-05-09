971ca464ee3b1233c1971e4651fb5cefaa030be1
Maxence <43523385+MaxenceBgn@users.noreply.github.com>
2023-05-09 15:21:51 +0200

    Creation du projet

---
+++ Applications web/MvcMovie/Domain/DTO/Movie/MovieItemDTO.cs
@@ -0,0 +1,24 @@
+﻿using MvcMovie.Models;
+
+namespace MvcMovie.Domain.DTO
+{
+    public class MovieItemDTO
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Gender { get; set; }
+
+        public MovieItemDTO()
+        {
+        }
+
+        public MovieItemDTO(Movie movie)
+        {
+            ID = movie.Id;
+            Title = movie.Title;
+            Gender = movie.Genre;
+        }
+
+        
+    }
+}
---
+++ Applications web/MvcMovie/Domain/Services/MovieService.cs
@@ -0,0 +1,28 @@
+﻿using MvcMovie.Domain.DTO;
+
+namespace MvcMovie.Domain.Services
+{
+    public class MovieService
+    {
+        private MvcMovie.Data.MvcMovieContext _context;
+
+        public MovieService(Data.MvcMovieContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<MovieItemDTO> GetMovieItemDTOs() 
+        {
+            var result = new List<MovieItemDTO>();
+            var movies = _context.Movie.ToList();
+            movies.ForEach(movie => 
+                {
+                    var dto = new MovieItemDTO(movie);
+                    result.Add(dto);
+                });
+
+            var re = movies.Select(movie => new MovieItemDTO(movie)).ToList();
+            return re;
+        }
+    }
+}
