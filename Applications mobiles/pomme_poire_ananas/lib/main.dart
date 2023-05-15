9043c67009dffd986fb5e41cb58b837705c89309
Maxence <43523385+MaxenceBgn@users.noreply.github.com>
2023-05-15 13:59:50 +0200

    - Ajout des images
    - Tous les éléments sont affichés sous forme de liste scrollable
    - Les éléments s'incrémentent automatiquement les uns en dessous des autres
    - Ajout des images dans le dossier assets/images
    - Ajout des images dans la liste, en fonction du nombre qui leur correspond (nombre premier, nombre pair et nombre impair)

--- Applications mobiles/pomme_poire_ananas/lib/main.dart
+++ Applications mobiles/pomme_poire_ananas/lib/main.dart
@@ -30,12 +30,40 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  static String title = "Pomme, Poire et Ananas";
+  static String title = "Cc";
   List<int> items = [1, 2, 3, 4, 5];
+  List<int> primeNumber = [
+    2,
+    3,
+    5,
+    7,
+    11,
+    13,
+    17,
+    19,
+    23,
+    29,
+    31,
+    37,
+    41,
+    47,
+    53,
+    59,
+    61,
+    67,
+    71,
+    73,
+    79,
+    83,
+    89,
+    97
+  ];
+  int counter = 0;
 
   @override
   void initState() {
     super.initState();
+    counter = items.length;
   }
 
   @override
@@ -43,7 +71,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(_MyHomePageState.title),
+        title: Text(title),
       ),
       body: Column(
         children: [
@@ -51,7 +79,30 @@ class _MyHomePageState extends State<MyHomePage> {
             shrinkWrap: true,
             itemCount: items.length,
             itemBuilder: (context, index) {
+              MaterialColor _tileColor;
+              if (items[index] % 2 == 0) {
+                _tileColor = Colors.blue;
+              } else {
+                _tileColor = Colors.indigo;
+              }
+              String _asset;
+              if (primeNumber.contains(items[index])) {
+                _asset = "images/ananas.png";
+                title = "Ananas";
+              } else {
+                if (items[index] % 2 == 0) {
+                  _asset = "images/poire.png";
+                  title = "Poire";
+                } else {
+                  _asset = "images/pomme.png";
+                  title = "Pomme";
+                }
+              }
+
               return ListTile(
+                tileColor: _tileColor,
+                textColor: Colors.white,
+                leading: Image.asset(_asset),
                 title: Text(items[index].toString()),
               );
             },
@@ -63,7 +114,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 items.add(counter);
               });
             },
-            child: Text('Increment'),
+            child: Text('Nouveau fruit'),
           ),
         ],
       ),
