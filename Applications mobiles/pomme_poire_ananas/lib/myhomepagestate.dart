50ce4bfb389b6d4abb3e3d182e4b364399e80372
Maxence <43523385+MaxenceBgn@users.noreply.github.com>
2023-05-15 16:31:48 +0200

    - Pomme, poire et ananas - Modification du bouton
    - Modification du design du bouton et ajout d'une icone plutôt qu'un texte
    - Fixation du bouton en bas de l'écran (ne se déplace plus en fonction de la longueur de la liste)

--- Applications mobiles/pomme_poire_ananas/lib/myhomepagestate.dart
+++ Applications mobiles/pomme_poire_ananas/lib/myhomepagestate.dart
@@ -49,57 +49,132 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   @override
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(_title),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            ListView.builder(
-              shrinkWrap: true,
-              itemCount: items.length,
-              itemBuilder: (context, index) {
-                MaterialColor tileColor;
-                if (items[index] % 2 == 0) {
-                  tileColor = Colors.blue;
-                } else {
-                  tileColor = Colors.indigo;
-                }
-                String asset;
-                if (verifyIfNumberIsPrime(items[index])) {
-                  asset = "images/ananas.png";
-                } else {
-                  if (items[index] % 2 == 0) {
-                    asset = "images/poire.png";
-                  } else {
-                    asset = "images/pomme.png";
-                  }
-                }
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: items.length,
+                    itemBuilder: (context, index) {
+                      MaterialColor tileColor;
+                      if (items[index] % 2 == 0) {
+                        tileColor = Colors.blue;
+                      } else {
+                        tileColor = Colors.indigo;
+                      }
+                      String asset;
+                      if (verifyIfNumberIsPrime(items[index])) {
+                        asset = "images/ananas.png";
+                      } else {
+                        if (items[index] % 2 == 0) {
+                          asset = "images/poire.png";
+                        } else {
+                          asset = "images/pomme.png";
+                        }
+                      }
 
-                return ListTile(
-                  tileColor: tileColor,
-                  textColor: Colors.white,
-                  leading: Image.asset(asset),
-                  title: Text(items[index].toString()),
-                );
-              },
+                      return ListTile(
+                        tileColor: tileColor,
+                        textColor: Colors.white,
+                        leading: Image.asset(asset),
+                        title: Text(items[index].toString()),
+                      );
+                    }),
+              ],
             ),
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  int counter = items.length + 1;
-                  items.add(counter);
-                  updateTitle();
-                });
-              },
-              child: const Text('Nouveau fruit'),
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: Padding(
+              padding: const EdgeInsets.only(
+                  right: 30, bottom: 30), // 30 pixels du bord droit et bas
+              child: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    int counter = items.length + 1;
+                    items.add(counter);
+                    updateTitle();
+                  });
+                },
+                style: ElevatedButton.styleFrom(
+                  shape: const CircleBorder(),
+                  padding: const EdgeInsets.all(20),
+                  backgroundColor: Colors.red[400], // <-- Couleur du bouton
+                ),
+                child: const Icon(Icons.add, color: Colors.white),
+              ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
+
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     appBar: AppBar(
+  //       backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+  //       title: Text(_title),
+  //     ),
+  //     body: SingleChildScrollView(
+  //       child: Column(
+  //         children: [
+  //           ListView.builder(
+  //             shrinkWrap: true,
+  //             itemCount: items.length,
+  //             itemBuilder: (context, index) {
+  //               MaterialColor tileColor;
+  //               if (items[index] % 2 == 0) {
+  //                 tileColor = Colors.blue;
+  //               } else {
+  //                 tileColor = Colors.indigo;
+  //               }
+  //               String asset;
+  //               if (verifyIfNumberIsPrime(items[index])) {
+  //                 asset = "images/ananas.png";
+  //               } else {
+  //                 if (items[index] % 2 == 0) {
+  //                   asset = "images/poire.png";
+  //                 } else {
+  //                   asset = "images/pomme.png";
+  //                 }
+  //               }
+
+  //               return ListTile(
+  //                 tileColor: tileColor,
+  //                 textColor: Colors.white,
+  //                 leading: Image.asset(asset),
+  //                 title: Text(items[index].toString()),
+  //               );
+  //             },
+  //           ),
+  //           ElevatedButton(
+  //             onPressed: () {
+  //               setState(() {
+  //                 int counter = items.length + 1;
+  //                 items.add(counter);
+  //                 updateTitle();
+  //               });
+  //             },
+  //             style: ElevatedButton.styleFrom(
+  //               shape: const CircleBorder(),
+  //               padding: const EdgeInsets.all(20),
+  //               backgroundColor: Colors.red[400], // <-- Button color
+  //             ),
+  //             child: const Icon(Icons.add, color: Colors.white),
+  //           )
+  //         ],
+  //       ),
+  //     ),
+  //   );
+  // }
 }
