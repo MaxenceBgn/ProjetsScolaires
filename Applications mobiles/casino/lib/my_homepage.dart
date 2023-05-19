60afa485483eba8585ce407852555abd34f198be
Maxence <43523385+MaxenceBgn@users.noreply.github.com>
2023-05-19 10:06:08 +0200

    - Projet casino - Ajout de la barre de slots
    - Ajout de la barre de slot
    - Affichage de 3 images, qui changent en fonction du jeu
    - Ajout de la fonctionnalité "lancer" lorsqu'on clique sur le bouton de jeu, qui génère au hasard 3 images dans la barre de slots

--- Applications mobiles/casino/lib/my_homepage.dart
+++ Applications mobiles/casino/lib/my_homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -10,11 +11,49 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  int _slot1 = 0;
+  int _slot2 = 0;
+  int _slot3 = 0;
+  Image _imageSlot1 = Image.asset('images/cerise.png', width: 100, height: 100);
+  Image _imageSlot2 = Image.asset('images/cerise.png', width: 100, height: 100);
+  Image _imageSlot3 = Image.asset('images/cerise.png', width: 100, height: 100);
 
   void _incrementCounter() {
     setState(() {
       _counter++;
     });
+  }
+
+  void _definiteSlotsImages() {
+    var rng = Random();
+    _slot1 = rng.nextInt(6);
+    _slot2 = rng.nextInt(6);
+    _slot3 = rng.nextInt(6);
+
+    setState(() {
+      _imageSlot1 = _getImageForSlot(_slot1);
+      _imageSlot2 = _getImageForSlot(_slot2);
+      _imageSlot3 = _getImageForSlot(_slot3);
+    });
+  }
+
+  Image _getImageForSlot(int slot) {
+    switch (slot) {
+      case 0:
+        return Image.asset('images/bar.png', width: 100, height: 100);
+      case 1:
+        return Image.asset('images/cerise.png', width: 100, height: 100);
+      case 2:
+        return Image.asset('images/cloche.png', width: 100, height: 100);
+      case 3:
+        return Image.asset('images/diamant.png', width: 100, height: 100);
+      case 4:
+        return Image.asset('images/fer-a-cheval.png', width: 100, height: 100);
+      case 5:
+        return Image.asset('images/pasteque.png', width: 100, height: 100);
+      default:
+        return Image.asset('images/sept.png', width: 100, height: 100);
+    }
   }
 
   @override
@@ -29,8 +68,16 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                _imageSlot1,
+                const SizedBox(width: 50),
+                _imageSlot2,
+                const SizedBox(width: 50),
+                _imageSlot3,
+              ],
             ),
             Text(
               '$_counter',
@@ -40,10 +87,10 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: _definiteSlotsImages,
         tooltip: 'Lancer',
-        child: const Icon(Icons.casino),
         backgroundColor: Colors.red[600],
+        child: const Icon(Icons.casino),
       ),
     );
   }
