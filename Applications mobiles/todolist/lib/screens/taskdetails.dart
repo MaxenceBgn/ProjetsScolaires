c6ad09423bfb5ef6551bf9bb01dc398f224d2535
Maxence <43523385+MaxenceBgn@users.noreply.github.com>
2023-06-15 16:22:16 +0200

    Mise à jour de la page de détails - Todolist
    - Amélioration visuelle de l'interface de détail d'une détail
    - Ajout d'un bouton icone permettant de supprimer directement une tâche depuis le détail
    - Redirection automatique vers la liste raffraichie des tâches

--- Applications mobiles/todolist/lib/screens/taskdetails.dart
+++ Applications mobiles/todolist/lib/screens/taskdetails.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:todolist/models/task.dart';
 import 'package:intl/intl.dart';
+import 'package:todolist/providers/taskprovider.dart';
+import 'package:todolist/screens/taskmaster.dart';
 
 class TaskDetails extends StatefulWidget {
   const TaskDetails({Key? key, required this.task}) : super(key: key);
   final Task task;
 
   @override
+  // ignore: library_private_types_in_public_api
   _TaskDetailsState createState() => _TaskDetailsState();
 }
 
@@ -18,18 +22,90 @@ class _TaskDetailsState extends State<TaskDetails> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text("Détail d'une tâche"),
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Text(widget.task.title),
-            Text("Id tâche : ${widget.task.id}"),
-            const Text("Créateur de la tâche : Maxence BOURGIN"),
             Text(
-                "Créée le ${DateFormat('dd.MM.yyyy').format(widget.task.createdAt)}"),
+              widget.task.title,
+              style: const TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 12),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                IconButton(
+                  onPressed: () {
+                    Provider.of<TaskProvider>(context, listen: false)
+                        .deleteTask(widget.task);
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const TasksMaster(),
+                      ),
+                    );
+                  },
+                  icon: const Icon(
+                    Icons.delete,
+                    color: Colors.red,
+                  ),
+                ),
+                const SizedBox(width: 16),
+                IconButton(
+                  onPressed: () {
+                    // Action à effectuer lors du clic sur l'icône "modifier"
+                  },
+                  icon: const Icon(Icons.edit),
+                ),
+              ],
+            ),
+            const SizedBox(height: 12),
             Text(
-                "DeadLine : tâche à effectuer pour le ${DateFormat('dd.MM.yyyy').format(widget.task.deadLine)}"),
-            Text("Description de la tâche : ${widget.task.content}")
+              "ID de la tâche : ${widget.task.id}",
+              style: const TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            const SizedBox(height: 8),
+            const Text(
+              "Créateur de la tâche : Maxence BOURGIN",
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              "Créée le ${DateFormat('dd.MM.yyyy').format(widget.task.createdAt)}",
+              style: const TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              "Date limite : ${DateFormat('dd.MM.yyyy').format(widget.task.deadLine)}",
+              style: const TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            const SizedBox(height: 12),
+            const Text(
+              "Description de la tâche :",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              widget.task.content,
+              style: const TextStyle(
+                fontSize: 16,
+              ),
+            ),
           ],
         ),
       ),
