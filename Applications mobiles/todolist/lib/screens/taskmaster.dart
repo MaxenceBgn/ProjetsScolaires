59722ed438988be19739b2930dc628a37251e872
Maxence <43523385+MaxenceBgn@users.noreply.github.com>
2023-06-15 14:39:35 +0200

    Ajout de la persistance des tâches Todolist
    - Ajout du provider pour les tâches
    - Persistance des tâches dans toutes les pages de l'application

--- Applications mobiles/todolist/lib/screens/taskmaster.dart
+++ Applications mobiles/todolist/lib/screens/taskmaster.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:todolist/models/task.dart';
+import 'package:todolist/providers/taskprovider.dart';
 import 'package:todolist/screens/taskdetails.dart';
 
 class TasksMaster extends StatefulWidget {
-  const TasksMaster({super.key, required this.tasks});
-
-  final List<Task> tasks;
+  const TasksMaster({super.key});
 
   @override
   State<TasksMaster> createState() => _TaskMasterPageState();
@@ -17,37 +17,45 @@ class _TaskMasterPageState extends State<TasksMaster> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text("Affichage des tâches"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Expanded(
-              child: ListView.builder(
-                itemCount: widget.tasks.length,
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text("Affichage des tâches"),
+        ),
+        body: Consumer<TaskProvider>(builder: (context, taskProvider, _) {
+          List<Task> tasks = taskProvider.tasks;
+
+          return Column(
+            children: [
+              ListView.builder(
+                shrinkWrap: true,
+                itemCount: tasks.length,
                 itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(widget.tasks[index].title),
+                  return InkWell(
                     onTap: () {
-                      // Naviguer vers la page de détails
+                      Task selectedTask = tasks[index];
                       Navigator.push(
                         context,
                         MaterialPageRoute(
-                          builder: (context) =>
-                              TaskDetails(task: widget.tasks[index]),
+                          builder: (context) => TaskDetails(
+                            task: selectedTask,
+                          ),
                         ),
                       );
                     },
+                    child: ListTile(
+                      tileColor: Colors.blueAccent,
+                      textColor: Colors.white,
+                      title: Row(
+                        children: [
+                          Text(tasks[index].title),
+                        ],
+                      ),
+                    ),
                   );
                 },
               ),
-            ),
-          ],
-        ),
-      ),
-    );
+            ],
+          );
+        }));
   }
 }
--- Applications mobiles/todolist/lib/todolistApp.dart
+++ Applications mobiles/todolist/lib/todolistApp.dart
@@ -1,23 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:todolist/models/task.dart';
+import 'package:provider/provider.dart';
+import 'package:todolist/providers/taskprovider.dart';
 import 'package:todolist/screens/taskmaster.dart';
 
 class TodolistApp extends StatelessWidget {
-  const TodolistApp({Key? key, required this.tasks}) : super(key: key);
-
-  final List<Task> tasks;
+  const TodolistApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'TodoList',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      debugShowCheckedModeBanner: false,
-      home: TasksMaster(
-        tasks: tasks,
+    return ChangeNotifierProvider(
+      create: (context) => TaskProvider(),
+      child: MaterialApp(
+        title: 'TodoList',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        debugShowCheckedModeBanner: false,
+        home: const TasksMaster(),
       ),
     );
   }
