490958d144c8f8af89f3d6ee60be9cee5b3e0c8f
Maxence <43523385+MaxenceBgn@users.noreply.github.com>
2023-06-22 10:35:59 +0200

    Amélioration UI fiche produit - EMarket
    - Affichage complet du détail des produits
    - Affichage variable du nombre de produits en stock (la couleur varie entre rouge, orange et vert en fonction du stock disponible)

--- Applications mobiles/emarket/lib/screens/product_detail_page.dart
+++ Applications mobiles/emarket/lib/screens/product_detail_page.dart
@@ -11,6 +11,16 @@ class ProductDetailPage extends StatefulWidget {
 }
 
 class _ProductDetailPageState extends State<ProductDetailPage> {
+  Color stockColor() {
+    if (widget.product.stock == 0) {
+      return Colors.red;
+    } else if (widget.product.stock < 20) {
+      return Colors.yellow[800]!;
+    } else {
+      return const Color.fromARGB(255, 1, 148, 6);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,9 +30,51 @@ class _ProductDetailPageState extends State<ProductDetailPage> {
       ),
       body: Center(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text(widget.product.description),
-            // Ajoutez d'autres widgets pour afficher les détails du produit ici
+            Image.network(
+              widget.product.images[0],
+              width: 300,
+              height: 300,
+            ),
+            Text(
+              widget.product.title,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'Catégorie: ${widget.product.category}',
+              style: const TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'Description: ${widget.product.description}',
+              style: const TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              '${widget.product.price} €',
+              style: const TextStyle(
+                color: Colors.blue,
+                fontSize: 16,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'Stock: ${widget.product.stock}',
+              style: TextStyle(
+                color: stockColor() ??
+                    Colors.black, // Utilisation de l'opérateur de null-aware
+                fontSize: 16,
+              ),
+            ),
           ],
         ),
       ),
