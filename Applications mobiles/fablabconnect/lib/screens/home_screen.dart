fe1702c4f7bb67e061eaa8c8a13a78e7ac2d283d
Maxence <43523385+MaxenceBgn@users.noreply.github.com>
2023-06-28 09:34:59 +0200

    Connexion à la base de données - Fablab Auth
    - Connexion du projet flutter à la base de données
    - Affichage de tous les utilisateurs sous forme de liste

--- Applications mobiles/fablabconnect/lib/screens/home_screen.dart
+++ Applications mobiles/fablabconnect/lib/screens/home_screen.dart
@@ -1,48 +1,58 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key, required this.title});
+  const HomeScreen({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int _counter = 0;
+  List<dynamic> users = []; // Liste pour stocker les utilisateurs récupérés
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    fetchData(); // Appel de la fonction pour récupérer les données au chargement de l'écran
+  }
+
+  Future<void> fetchData() async {
+    final response = await http
+        .get(Uri.parse('http://localhost:3000/api/utilisateurs/distinct'));
+    if (response.statusCode == 200) {
+      // Vérifiez si la requête a réussi (code de statut 200)
+      final jsonData = jsonDecode(response.body);
+      setState(() {
+        users =
+            jsonData; // Met à jour la liste des utilisateurs avec les données récupérées
+      });
+    } else {
+      // Gère les erreurs de requête en conséquence
+      print(
+          'Erreur lors de la récupération des données : ${response.statusCode}');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        backgroundColor: Theme.of(context).colorScheme.secondary,
+        title: const Text("Liste des utilisateurs"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      body: ListView.builder(
+        itemCount: users.length,
+        itemBuilder: (BuildContext context, int index) {
+          final user = users[index];
+          return ListTile(
+            title: Text(user['Nom']),
+            subtitle: Text(user['Prenom']),
+          );
+        },
       ),
     );
   }
