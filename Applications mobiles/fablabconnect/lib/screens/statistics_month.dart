742bc432c97b8e63d0f641bc0dc45ebc3a544c38
Maxence <43523385+MaxenceBgn@users.noreply.github.com>
2023-07-07 13:01:40 +0200

    Fablab-Auth // Ajout téléchargement PDF pour statistiques mensuels
    - Ajout d'un bouton de téléchargement sur la page des statistiques mensuels
    - Possibilité de télécharger un PDF avec les statistiques complètes du mois en cours
    - Le nom du fichier PDF comporte désormais le mois et l'année en cours

--- Applications mobiles/fablabconnect/lib/screens/statistics_month.dart
+++ Applications mobiles/fablabconnect/lib/screens/statistics_month.dart
@@ -1,13 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:syncfusion_flutter_pdf/pdf.dart';
+import 'package:fablabconnect/save_file_web.dart';
 
 // ignore: must_be_immutable
 class MonthStatsScreen extends StatelessWidget {
   List<dynamic> users;
   int percentageMale = 0;
+  int numberOfMales = 0;
   int averageAge = 0;
   int totalConnexionNumber = 0;
   String totalConnexionHours = '';
+  int year = 0;
+  int monthInt = 0;
+  String monthString = "";
+  int userNumber = 0;
+  int ageMin = 100;
+  int ageMax = 0;
+  int ageMinFemale = 100;
+  int ageMaxFemale = 0;
+  int ageMinMale = 100;
+  int ageMaxMale = 0;
+  int averageAgeFemale = 0;
+  int averageAgeMale = 0;
 
   MonthStatsScreen({super.key, required this.users});
 
@@ -30,22 +45,55 @@ class MonthStatsScreen extends StatelessWidget {
     return '$formattedHours $hoursText et $formattedMinutes $minutesText.';
   }
 
+  //Calcule la moyenne d'âge des utilisateurs et initialisation des variables d'âge
   int calculateAverageAge() {
-    //Calcule la moyenne d'âge des utilisateurs
     List<int> ages = [];
+    List<int> agesFemale = [];
+    List<int> agesMale = [];
     for (var i = 0; i < users.length; i++) {
       ages.add(users[i]['Age']);
+      //Tous les utilisateurs
+      if (users[i]['Age'] < ageMin) {
+        ageMin = users[i]['Age'];
+      }
+      if (users[i]['Age'] > ageMax) {
+        ageMax = users[i]['Age'];
+      }
+      if (users[i]['Sexe'] == 'H') {
+        //Utilisateurs hommes
+        if (users[i]['Age'] < ageMinMale) {
+          ageMinMale = users[i]['Age'];
+        }
+        if (users[i]['Age'] > ageMaxMale) {
+          ageMaxMale = users[i]['Age'];
+        }
+        agesMale.add(users[i]['Age']);
+      } else {
+        //Utilisateurs femmes
+        if (users[i]['Age'] < ageMinFemale) {
+          ageMinFemale = users[i]['Age'];
+        }
+        if (users[i]['Age'] > ageMaxFemale) {
+          ageMaxFemale = users[i]['Age'];
+        }
+        agesFemale.add(users[i]['Age']);
+      }
     }
 
     double sum = ages.reduce((value, element) => value + element).toDouble();
     double average = sum / ages.length;
 
+    sum = agesMale.reduce((value, element) => value + element).toDouble();
+    averageAgeMale = sum ~/ agesMale.length;
+
+    sum = agesFemale.reduce((value, element) => value + element).toDouble();
+    averageAgeFemale = sum ~/ agesFemale.length;
+
     return average.toInt();
   }
 
+  //Calcule le pourcentage d'hommes
   int returnPercentageOfMale() {
-    //Calcule le pourcentage d'hommes
-    int numberOfMales = 0;
     double percent = 0;
     for (var i = 0; i < users.length; i++) {
       if (users[i]['Sexe'] == "H") {
@@ -74,24 +122,120 @@ class MonthStatsScreen extends StatelessWidget {
     return connectionHours;
   }
 
+  //Retourne la liste des utilisateurs avec le filtre (ici, cela ne concerne que les utilisateurs dont la dernière connexion date de l'année en cours)
   List<dynamic> returnMonthUserList() {
-    //On fait le tri dans la liste des users, on ne garde que ceux qui se sont connectés au moins une fois ce mois-ci
     List<dynamic> usersMonth = [];
 
     DateTime now = DateTime.now();
-    DateTime firstDayOfMonth = DateTime(now.year, now.month, 1);
+    DateTime firstDayOfYear = DateTime(now.year, now.month, 1);
 
     if (users.isNotEmpty) {
       for (var i = 0; i < users.length; i++) {
         DateTime lastConnection = DateTime.parse(users[i]['DerniereConnexion']);
-        if (lastConnection.isAfter(firstDayOfMonth) ||
-            lastConnection.isAtSameMomentAs(firstDayOfMonth)) {
+        if (lastConnection.isAfter(firstDayOfYear) ||
+            lastConnection.isAtSameMomentAs(firstDayOfYear)) {
           usersMonth.add(users[i]);
         }
       }
     }
-
     return usersMonth;
+  }
+
+  String returnMonthString() {
+    String month = "";
+    switch (monthInt) {
+      case 1:
+        month = "Janvier";
+        break;
+      case 2:
+        month = "Janvier";
+        break;
+      case 3:
+        month = "Janvier";
+        break;
+      case 4:
+        month = "Janvier";
+        break;
+      case 5:
+        month = "Janvier";
+        break;
+      case 6:
+        month = "Janvier";
+        break;
+      case 7:
+        month = "Janvier";
+        break;
+      case 8:
+        month = "Janvier";
+        break;
+      case 9:
+        month = "Janvier";
+        break;
+      case 10:
+        month = "Janvier";
+        break;
+      case 11:
+        month = "Janvier";
+        break;
+      case 12:
+        month = "Janvier";
+        break;
+      default:
+        month = "Erreur : aucun mois n'a pu être défini.";
+    }
+    return month;
+  }
+
+  //Création du PDF résumant les statistiques de l'année en cours
+  Future<void> _createPDF() async {
+    //Créé un document PDF
+    var document = PdfDocument();
+
+    //PERSONNALISATION DU PDF ///////////////////////////////////////////////////////////////////////////////////////////////////
+
+    PdfPage page = document.pages.add();
+
+    // Titre en haut
+    String title = "Statistiques de $monthString ${year.toString()}";
+    final font = PdfStandardFont(PdfFontFamily.helvetica, 24);
+
+    final titleSize = font.measureString(title);
+    final titleX = (page.getClientSize().width - titleSize.width) / 2;
+    double titleY = 0;
+
+    page.graphics.drawString(
+      title,
+      font,
+      brush: PdfSolidBrush(PdfColor(0, 0, 0)),
+      bounds: Rect.fromLTWH(titleX, titleY, titleSize.width, titleSize.height),
+    );
+
+// Corps du PDF
+    double textElementX = 0;
+    final textElementY = titleSize.height + 10;
+
+    PdfTextElement textElement = PdfTextElement(
+      text:
+          "Répartition des utilisateurs : \nNombre d'utilisateurs s'étant connectés au moins une fois en ${year.toString()} : ${userNumber.toString()}.\n$numberOfMales (${percentageMale.toString()}%) sont des hommes, et ${(userNumber - numberOfMales).toString()} (${(100 - percentageMale).toString()}%) sont des femmes.\n\n  \nÂge des utilisateurs : \nMoyenne d'âge des utilisateurs : ${averageAge.toString()} ans (le plus jeune a $ageMin et le plus âgé a $ageMax). \nHommes : la moyenne d'âge est de ${averageAgeMale.toString()}, le plus jeune a $ageMinMale ans et le plus ancien a $ageMaxMale \nFemmes : la moyenne d'âge est de ${averageAgeFemale.toString()}, le plus jeune a $ageMinFemale ans et le plus ancien a $ageMaxFemale",
+      font: PdfStandardFont(PdfFontFamily.helvetica, 12),
+    );
+    textElement.draw(
+      page: page,
+      bounds: Rect.fromLTWH(
+          textElementX,
+          textElementY,
+          page.getClientSize().width,
+          page.getClientSize().height - titleSize.height - 10),
+    );
+
+// Sauvegarde
+    List<int> bytes = await document.save();
+
+    document.dispose();
+
+// Enregistrement + téléchargement du PDF
+    SaveFile.saveAndLaunchFile(
+        bytes, 'statistiques_${monthString}_${year.toString()}.pdf');
   }
 
   void initializeVariables() {
@@ -102,12 +246,16 @@ class MonthStatsScreen extends StatelessWidget {
     totalConnexionNumber = returnTotalConnectionNumber();
     totalConnexionHours =
         formatConnectionTime(calculateNumberOfHoursConnection());
+    DateTime now = DateTime.now();
+    year = now.year;
+    userNumber = users.length;
+    monthInt = now.month;
+    monthString = returnMonthString();
   }
 
   @override
   Widget build(BuildContext context) {
     initializeVariables();
-    final int userNumber = users.length;
     final String userNumberString = userNumber.toString();
 
     return Scaffold(
@@ -202,6 +350,12 @@ class MonthStatsScreen extends StatelessWidget {
                           Text(
                             totalConnexionHours.toString(),
                             style: const TextStyle(fontSize: 25.0),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {
+                              _createPDF();
+                            },
+                            child: const Text('Générer PDF'),
                           ),
                         ],
                       ),
